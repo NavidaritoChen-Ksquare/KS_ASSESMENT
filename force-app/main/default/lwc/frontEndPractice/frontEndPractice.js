f88570c3a3f607aa92a4df7d3a93c2c23c90d8a2
Navidarito Chen <Navidarito.Chen@itksquare.edu.mx>
2023-02-01 15:03:45 -0400

    Uploading frontEndPractice

--- force-app/main/default/lwc/frontEndPractice/frontEndPractice.js
+++ force-app/main/default/lwc/frontEndPractice/frontEndPractice.js
@@ -1,26 +1,37 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, track, wire, api } from 'lwc';
 import displayContactRecords  from '@salesforce/apex/ContactController.displayContactRecords';
 
 export default class FrontEndPractice extends LightningElement {
+    @api recordId;
+    conValue;
     @track conName;
     @track records;
     @track error;
-    @wire(displayContactRecords,{searchKey:'$conName'})
+    
+    @wire(displayContactRecords,{searchKey:'$conName',accId:'$recordId'})
     wiredContact({error,data}){
         if(data){
             this.records=data;
             this.error=undefined;
-            console.log(data);
         }
         else{
             this.error=error;
             this.records=undefined;
-            console.log('no data');
         }
     }
 
     handleChange(event){
-        this.conName=event.target.value;
-        console.log('Contact Name '+this.conName);
+        this.conValue=event.target.value;
+        if(event.target.value.length>=3){
+            this.conName=event.target.value;
+        }
+    }
+    handleKeyPress(event){
+        if(event.keyCode==13){
+            this.conName=event.target.value;
+        }
+    }
+    handleClick(event){
+        this.conName=this.conValue;
     }
 }
